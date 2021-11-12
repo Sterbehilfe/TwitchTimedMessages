dc5e59ec660df90baacf45c2e0684768b56c8162
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2021-11-12 16:26:31 +0100

    added JoinChannels() and _timers

--- TwitchTimedMessages/Twitch/TwitchClient.go
+++ TwitchTimedMessages/Twitch/TwitchClient.go
@@ -2,6 +2,7 @@ package twitch
 
 import (
 	settings "Documents/Git/TwitchTimedMessages/TwitchTimedMessages/Settings"
+	"time"
 
 	linq "github.com/ahmetb/go-linq"
 	irc "github.com/gempir/go-twitch-irc"
@@ -10,19 +11,19 @@ import (
 type TwitchClient struct {
 	_settings settings.Settings
 	_client   *irc.Client
+	_timers   []time.Timer
 }
 
 func NewTwitchClient(settings settings.Settings) *TwitchClient {
 	return &TwitchClient{
 		_settings: settings,
 		_client:   irc.NewClient(settings.Username, settings.OAuthToken),
+		_timers:   make([]time.Timer, len(settings.Messages)),
 	}
 }
 
 func (client *TwitchClient) Initialize() {
-	for _, channel := range client.GetChannels() {
-		client._client.Join(channel)
-	}
+	client.JoinChannels()
 }
 
 func (client *TwitchClient) GetChannels() []string {
@@ -31,4 +32,10 @@ func (client *TwitchClient) GetChannels() []string {
 		return m.Channel
 	}).Distinct().ToSlice(&result)
 	return result
+}
+
+func (client *TwitchClient) JoinChannels() {
+	for _, channel := range client.GetChannels() {
+		client._client.Join(channel)
+	}
 }
