8d677b4890e5e7b96d0fc5561db26ade0d5587bb
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2021-11-12 15:04:06 +0100

    created GetChannels()

--- TwitchTimedMessages/Twitch/TwitchClient.go
+++ TwitchTimedMessages/Twitch/TwitchClient.go
@@ -2,18 +2,33 @@ package twitch
 
 import (
 	settings "Documents/Git/TwitchTimedMessages/TwitchTimedMessages/Settings"
+
+	linq "github.com/ahmetb/go-linq"
+	irc "github.com/gempir/go-twitch-irc"
 )
 
 type TwitchClient struct {
 	_settings settings.Settings
+	_client   *irc.Client
 }
 
 func NewTwitchClient(settings settings.Settings) *TwitchClient {
 	return &TwitchClient{
 		_settings: settings,
+		_client:   irc.NewClient(settings.Username, settings.OAuthToken),
 	}
 }
 
-func (client TwitchClient) Initialize() {
+func (client *TwitchClient) Initialize() {
+	for _, channel := range client.GetChannels() {
+		client._client.Join(channel)
+	}
+}
 
+func (client *TwitchClient) GetChannels() []string {
+	var result []string
+	linq.From(client._settings).SelectT(func(m settings.Message) string {
+		return m.Channel
+	}).Distinct().ToSlice(&result)
+	return result
 }
