e6da8b055136112d3b1c16d3c4e8e8413e05f7b4
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2021-12-10 16:13:17 +0100

    createad linux-arm and win-x64 build

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 cd ./TwitchTimedMessages
-go build -o ../Build/TwitchTimedMessages.exe ./main.go
+go env -w GOOS=linux GOARCH=arm
+go build -o ../Build/linux/TwitchTimedMessages ./main.go
+go env -w GOOS=windows GOARCH=amd64
+go build -o ../Build/win64/TwitchTimedMessages.exe ./main.go
 cd ..
-cp Settings.json Build/
+cp Settings.json Build/linux/
+cp Settings.json Build/win64/
