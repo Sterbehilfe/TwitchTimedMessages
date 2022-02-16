598fbf62fef059bfd999b841a83fd69121ccec16
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-02-16 20:41:43 +0100

    Delete my-build.sh

--- my-build.sh
+++
@@ -1,8 +0,0 @@
-cd TwitchTimedMessages/
-go env -w GOOS=linux GOARCH=arm
-go build -o ../Build/linux/TwitchTimedMessages ./main.go
-go env -w GOOS=windows GOARCH=amd64
-go build -o ../Build/win64/TwitchTimedMessages.exe ./main.go
-cd ..
-cp Settings.json Build/linux/
-cp Settings.json Build/win64/
